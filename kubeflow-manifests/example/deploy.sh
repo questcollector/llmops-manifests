13b1a43cf3eae33602de5e94838dd33fff45b2e4
kiyoung <kiyoung.you@samsung.com>
2024-06-13 18:36:14 +0900

    add manifests update shell script

---
+++ kubeflow-manifests/example/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+parent_path=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
+cd "$parent_path"
+
+## domain
+export DOMAIN=$(gcloud domains list-user-verified --format="value(id)")
+## cloud sql private ip
+export CLOUD_SQL_IP=$(cd ~/llmops-manifests/gke-terraform && terraform output -raw gcsCloudSqlPrivateIP)
+## oauth client id
+read -p "Enter Oauth Client ID: " client_id
+## oauth client secret id
+read -p "Enter Oauth Client Secret: " client_secret
+## user email id
+export EMAIL=$(gcloud config list core/account --format="value(core.account)")
+
+## istio
+sed -i 's/<<domain>>/'$DOMAIN'/g' ../common/istio-1-17/istio-install/overlays/gke-gateway/kubeflow-gateway.yaml
+## kserve
+sed -i 's/<<domain>>/'$DOMAIN'/' ../common/knative/knative-serving/overlays/gateways/patches/config-patch.yaml
+sed -i 's/<<domain>>/'$DOMAIN'/' ../common/knative/knative-serving/overlays/gateways/gateway.yaml
+## dex
+sed -i 's/<<domain>>/'$DOMAIN'/g' ../common/dex/overlays/google-oauth2-client/config-map.yaml
+sed -i 's/<<client_id>>/'$client_id'/g' ../common/dex/overlays/google-oauth2-client/config-map.yaml
+sed -i 's/<<client_secret>>/'$client_secret'/g' ../common/dex/overlays/google-oauth2-client/config-map.yaml
+## oidc-authservice
+sed -i 's/<<domain>>/'$DOMAIN'/g' ../common/oidc-client/oidc-authservice/base/params.env
+## katib
+sed -i 's/<<gcsCloudSqlPrivateIP>>/'$CLOUD_SQL_IP'/g' ../apps/katib/upstream/installs/katib-cert-manager-external-db/secrets.env
+## pipeline
+sed -i 's/<<gcsCloudSqlPrivateIP>>/'$CLOUD_SQL_IP'/g' ../apps/pipeline/upstream/env/platform-agnostic-multi-user/params.env
+## user profile
+sed -i 's/<<google_email>>/'$EMAIL'/g' ../common/user-namespace/base/params.env
