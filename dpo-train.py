38025c770f59316c60b64312d1320f012c3181df
kiyoung.you <miroirs01@gmail.com>
2024-09-06 02:48:17 +0900

    modify dpo-train.py

--- dpo-train.py
+++ dpo-train.py
@@ -10,10 +10,11 @@ from trl import (
 )
 from peft import LoraConfig, TaskType, PeftModel
 import torch
-import numpy as np
+import torch.distributed as dist
+import mlflow
 
 import os
-from datasets import load_from_disk
+from datasets import load_dataset
 from typing import Dict, Optional
 from dataclasses import dataclass, field
 
@@ -28,6 +29,9 @@ class ScriptArguments:
     model_path: Optional[str] = field(default="meta-llama/Llama-2-7b-hf", metadata={"help": "the model path"})
     sft_adapter_path: Optional[str] = field(default="meta-llama/Llama-2-7b-hf", metadata={"help": "the sft lora adapter path"})
     dataset_path: Optional[str] = field(default="lvwerra/stack-exchange-paired", metadata={"help": "the dataset path"})
+    training_job_name: Optional[str] = field(default="sft-train", metadata={"help": "training job name"})
+    training_job_id: Optional[str] = field(default="abcde", metadata={"help": "training job id"})
+    system_prompt: Optional[str] = field(default="system prompt", metadata={"help": "system prompt"})
 
     # LoraConfig
     lora_alpha: Optional[float] = field(default=16, metadata={"help": "the lora alpha parameter"})
@@ -51,38 +55,8 @@ tokenizer = AutoTokenizer.from_pretrained(script_args.model_path, trust_remote_c
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 
-SYSTEM = """당신은 원영적 사고를 하는 지능형 어시스턴트입니다. 원영적 사고란 단순 긍정적인 사고를 넘어 초월적인 긍정적 사고 방식이며, 자신에게 일어나는 모든 사건이 궁극적으로 긍정적인 결과로 귀결될 것이라는 확고한 낙관주의를 기반으로 두고 있습니다. 
-즉, 나에게 일어나는 모든 일은 결국 나에게 좋은 일이라는 것입니다. 다만, 부정적인 감정을 회피하고 거부하는 것이 아니며, 부정적인 현실과 감정도 긍정적인 결과에 이르는 과정으로 받아들인다는 차이점이 있습니다.
-예를 들어, 갑자기 비가 온 상황에서, 비가 왔지만 나는 춥지 않고 행복하다라며 춥다는 부정적인 감각을 부정하는 것이 아니라, 추운 것은 맞지만 오히려 운치있는 빗소리를 들을 수 있으니까 운이 좋다. 즉 궁극적으로 긍정적인 결과로 이르게 되는 원인으로 해석하는 것입니다.
-이어지는 문장에서 나타는 상황에 대해 원영적 사고로 해석하는 역할을 수행합니다. emoji를 적극적으로 사용합니다."""
-
-def return_prompt_and_responses(samples) -> Dict[str, str]:
-    ## apply chat template
-    prompt_messages = [
-        [
-            {"role": "system", "content": SYSTEM},
-            {"role": "user", "content": prompt}
-        ] for prompt in samples["prompt"]
-    ]
-    return {
-        "prompt": [ tokenizer.apply_chat_template(chat_template, tokenize=False) for chat_template in prompt_messages ],
-        "chosen": [ f"<|start_header_id|>assistant<|end_header_id|>\n\n{chosen}<|eot_id|>" for chosen in samples["chosen"]],
-        "rejected": [ f"<|start_header_id|>assistant<|end_header_id|>\n\n{rejected}<|eot_id|>" for rejected in samples["rejected"]],
-    }
-dataset = load_from_disk(script_args.dataset_path)
-# dataset = dataset['train'].train_test_split(test_size=10, train_size=100)
-dataset = dataset['train'].train_test_split(test_size=0.1)
-dataset = dataset.map(
-    return_prompt_and_responses, 
-    batched=True, 
-    remove_columns=dataset["train"].column_names
-)
-train_data, valid_data = (dataset['train'], dataset['test'])
-train_data = train_data.filter(
-    lambda r: len(r["prompt"]) + len(r["chosen"]) <= training_args.max_length
-    and len(r["prompt"]) + len(r["rejected"]) <= training_args.max_length
-)
-valid_data = valid_data.filter(
+dataset = load_dataset("json", data_files=script_args.dataset_path)['train']
+dataset = dataset.filter(
     lambda r: len(r["prompt"]) + len(r["chosen"]) <= training_args.max_length
     and len(r["prompt"]) + len(r["rejected"]) <= training_args.max_length
 )
@@ -98,16 +72,39 @@ model = PeftModel.from_pretrained(model, script_args.sft_adapter_path, is_traina
 
 model.load_adapter(script_args.sft_adapter_path, adapter_name=training_args.ref_adapter_name)
 
+## mlflow
+os.environ["MLFLOW_EXPERIMENT_NAME"] = script_args.training_job_name
+os.environ["MLFLOW_FLATTEN_PARAMS"] = "1"
+os.environ["MLFLOW_TRACKING_URI"]="http://mlflow.mlflow"
+os.environ["MLFLOW_S3_ENDPOINT_URL"]="http://minio-service.kubeflow:9000"
+os.environ["MLFLOW_S3_IGNORE_TLS"]="true"
+
+# create mlflow run in master
+if dist.get_rank() == 0:
+    mlflow.set_tracking_uri(os.environ["MLFLOW_TRACKING_URI"])
+    mlflow.set_experiment(os.environ["MLFLOW_EXPERIMENT_NAME"])
+    mlflow.start_run(run_name=script_args.training_job_id)
+    mlflow.log_param("system_prompt", script_args.system_prompt)
+
 trainer = DPOTrainer(
     args=training_args,
     model=model,
     tokenizer=tokenizer,
-    train_dataset=train_data,
-    eval_dataset=valid_data,
-    # peft_config=peft_config,
-    ref_model=None,
+    train_dataset=dataset,
 )
 
 trainer.model.print_trainable_parameters()
 trainer.train()
 trainer.save_model()
+
+## mlflow log model and end run
+if dist.get_rank() == 0:
+    mlflow.transformers.log_model(
+        transformers_model={
+            "model": trainer.model,
+            "tokenizer": tokenizer,
+        },
+        artifact_path="model",
+        task="text-generation"
+    )
+    mlflow.end_run()
