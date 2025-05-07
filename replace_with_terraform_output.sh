21c74c72e4ec25fb96f3e426b9066781a92398fa
kiyoung.you <miroirs01@gmail.com>
2025-05-07 21:21:26 +0900

    add aws access key in replacement shell script

--- replace_with_terraform_output.sh
+++ replace_with_terraform_output.sh
@@ -6,6 +6,10 @@ RDS_PORT=$(cd aws-terraform && terraform output -raw rds_port)
 S3_BUCKET=$(cd aws-terraform && terraform output -raw s3_bucket)
 RDS_PASSWORD=$(cd aws-terraform && terraform output -raw rds_password)
 
+# ⌨️ Access key, secret access key 입력
+read -p "Enter aws_access_key_id: " AWS_ACCESS_KEY_ID
+read -p -s "Enter aws_access_key_id: " AWS_SECRET_ACCESS_KEY
+
 # 🔍 치환 함수
 replace_placeholders() {
   infile="$1"
@@ -16,6 +20,8 @@ replace_placeholders() {
     line=$(echo "$line" | sed "s|YOUR_RDS_PORT|$RDS_PORT|g")
     line=$(echo "$line" | sed "s|YOUR_S3_BUCKET_NAME|$S3_BUCKET|g")
     line=$(echo "$line" | sed "s|YOUR_RDS_PASSWORD|$RDS_PASSWORD|g")
+    line=$(echo "$line" | sed "s|YOUR_AWS_ACCESS_ID|$AWS_ACCESS_KEY_ID|g")
+    line=$(echo "$line" | sed "s|YOUR_AWS_SECRET_KEY|$AWS_SECRET_ACCESS_KEY|g")
     printf "%s\n" "$line" >> "$tmpfile"
   done < "$infile"
 
@@ -27,6 +33,7 @@ for file in \
   kubeflow-manifests/apps/katib/upstream/installs/katib-cert-manager-external-db/secrets.env \
   kubeflow-manifests/apps/pipeline/upstream/env/platform-agnostic-multi-user/pipeline-install-config.yaml \
   kubeflow-manifests/apps/pipeline/upstream/env/platform-agnostic-multi-user/secret.env \
+  kubeflow-manifests/apps/pipeline/upstream/env/platform-agnostic-multi-user/minio-artifact-secret-patch.env \
   keycloak/values.yaml \
   mlflow/values.yaml \
   tests/mysql.yaml
